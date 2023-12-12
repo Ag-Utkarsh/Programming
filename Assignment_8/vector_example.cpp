c130daee7e770f22cdd19efa76db29a3623f70b7
Ag-Utkarsh <utkarshagarwal9641@gmail.com>
2023-12-12 14:02:36 +0530

    New files

---
+++ Assignment_8/vector_example.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main()
+{
+    vector<int> v(3);
+    cout<<"Size of vector: "<<v.size()<<endl; 
+    cout<<"Begin of vector: "<<v.begin()<<endl;  
+    return 0;
+    }  
