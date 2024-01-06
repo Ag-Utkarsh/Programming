a24a62f8940bf6d66224cd8ea6524035d7b5b0d8
Ag-Utkarsh <utkarshagarwal9641@gmail.com>
2024-01-06 20:41:35 +0530

    Added Leetcode Question Implement Stack Using Queue

---
+++ Python DSA questions/Implementing Stack Using Queue/README.md
@@ -0,0 +1,21 @@
+# Implementation of Stack Using Queue
+
+## Problem Statement
+
+Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+
+Implement the Stack class using queues as the internal data structure.
+
+## Example
+
+Input:
+["MyStack", "push", "push", "top", "pop", "empty"]
+[[], [1], [2], [], [], []]
+
+Output:
+[null, null, null, 2, 2, false]
+
+## Constraints
+
+- You must use only standard operations of a queue, which means only push to back, peek/pop from front, size, and is empty operations are valid.
+- Depending on your language, queue may not be supported natively. You may simulate a queue using a list or deque (double-ended queue), as long as you use only a queue's standard operations.
---
+++ Python DSA questions/Implementing Stack Using Queue/implementationOfStackUsingQueueMethod2.py
@@ -0,0 +1,51 @@
+'''
+Implement a last-in-first-out (LIFO) stack using only two queues. The implemented stack should support all the functions of a normal stack (push, top, pop, and empty).
+
+Implement the MyStack class:
+
+void push(int x) Pushes element x to the top of the stack.
+int pop() Removes the element on the top of the stack and returns it.
+int top() Returns the element on the top of the stack.
+boolean empty() Returns true if the stack is empty, false otherwise.
+Notes:
+
+You must use only standard operations of a queue, which means that only push to back, peek/pop from front, size and is empty operations are valid.
+Depending on your language, the queue may not be supported natively. You may simulate a queue using a list or deque (double-ended queue) as long as you use only a queue's standard operations.
+
+'''
+
+class MyStack:
+
+    def __init__(self):
+        self.q1 = deque()
+        self.q2 = deque()
+
+    def push(self, x: int) -> None:
+        self.q1.append(x)
+
+    def pop(self) -> int:
+        while len(self.q1) > 1:
+            self.q2.append(self.q1.popleft())
+        
+        popped_element = self.q1.popleft()
+        
+        # Swap q1 and q2
+        self.q1, self.q2 = self.q2, self.q1
+        
+        return popped_element
+
+    def top(self) -> int:
+        while len(self.q1) > 1:
+            self.q2.append(self.q1.popleft())
+        
+        top_element = self.q1[0]
+        
+        self.q2.append(self.q1.popleft())
+        
+        # Swap q1 and q2
+        self.q1, self.q2 = self.q2, self.q1
+        
+        return top_element
+
+    def empty(self) -> bool:
+        return len(self.q1) == 0
